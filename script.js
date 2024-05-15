a2eaffbe4df501fcf2c6908666488c28dbc279a4
Syefoyo <trisyefolionel@gmail.com>
2024-05-15 19:31:59 +0700

    morningincweb

---
+++ script.js
@@ -0,0 +1,5 @@
+function functionWork() {
+    document.getElementById("test").innerHTML = "Welcome To Our Official Website And Thanks To GitHub Who Has Make This Website Work,This Website Is Proggramed By The Owner Of Morning I.N.C Name Syefo Or You can Call Me By My profile Name called SyefoYo On GitHub"
+}
+let name = window.prompt("What IS Your Name?")
+document.write("Welcome" + " " + name)
